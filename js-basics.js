51c83fcd343e4900c258db7533f6b4992edddd63
Faith Makinde <faithmakinde0@gmail.com>
2023-02-16 02:10:13 +0100

    first commit

---
+++ js-basics.js
@@ -0,0 +1,12 @@
+// Alright here's an exercise for you, I want you to write a function that takes two numbers and returns the maximum of the two. Call that function, give a different argument, and make sure it's working properly.
+function max(num1, num2){
+    if (num1 > num2){
+        return num1;
+    }
+    else{
+        return num2
+    }
+}
+console.log(max(8, 10))
+
+// FizzBuzz is a task where the programmer is asked to print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and similarly print “Buzz” for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
