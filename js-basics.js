fb19b7d66fe734107e9f340baa9b1e31a70eb4b8
Faith Makinde <faithmakinde0@gmail.com>
2023-02-20 09:00:30 +0100

    work in progress

--- js-basics.js
+++ js-basics.js
@@ -1,12 +1,29 @@
-// Alright here's an exercise for you, I want you to write a function that takes two numbers and returns the maximum of the two. Call that function, give a different argument, and make sure it's working properly.
-function max(num1, num2){
-    if (num1 > num2){
-        return num1;
+// 3)Bolatito just got admitted to Greenfield High School as an Arts student. To commence her studies, she needs to know the subjects she will be taking as an Arts student. All students have to take the General subjects.
+// The subjects for each class group are as follows:
+// Science Subjects - Physics, Chemistry, Biology, Technical Drawing 
+// Social Science Subjects - Accounting, Commerce, Marketing, Geography
+// Arts Subjects - Government, Economics, Literature, History
+// General Subjects - English, Mathematics
+
+// Using if…else conditional statement, write a program that will help Bolatito determine the subjects she will be taking for the session using her class group. Also, in the case of an invalid class group, your output should be the General subjects. 
+
+function courseSubject(subject){
+    if(subject === 'science'){
+        return ['Physics', ' Chemistry', 'Biology', 'Technical Drawing '].join(', ')
+    }
+    else if(subject === 'socialScience'){
+        return ['Accounting', 'Commerce', 'Marketing', 'Geography'].join(' ,')
+    }
+    else if (subject === 'arts'){
+        return ['Government', 'Economics', 'Literature', 'History'].join(' ,')
+    }
+    else if (subject === 'general'){
+        return ['English', 'Mathematics'].join(' ,')
     }
     else{
-        return num2
+        return ['English', 'Mathematics'].join(' ,') 
     }
 }
-console.log(max(8, 10))
+ console.log(courseSubject('good'));
 
-// FizzBuzz is a task where the programmer is asked to print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and similarly print “Buzz” for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
+ 
